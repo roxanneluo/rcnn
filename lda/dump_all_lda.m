aa692e4928c5a4eae07c360614d2518c86efc7bd
roxanneluo <roxanneluoxuan@gmail.com>
2014-11-11 17:13:47 +0800

    add dump_all_lda

--- lda/dump_all_lda.m
+++ lda/dump_all_lda.m
@@ -4,7 +4,7 @@ ip.addRequired('feat_name',   @isstr);
 ip.addParamValue('max_num_per_class', 5500,@isscalar);
 ip.addParamValue('do_normalize',      true,@isscalar);
 ip.addParamValue('do_lda',      true,@isscalar);
-ip.parse(feat_name, num_filter, varargin{:});
+ip.parse(feat_name, varargin{:});
 opts = ip.Results;
 
 lda_dir = 'lda/';
@@ -21,15 +21,21 @@ proj_dim = cell_size_sum(trans,2);
 f_start = get_filter_start(trans);
 
 num_class = 20;
-matlabpool open
-parfor i = 1:num_class
+for i = 15:num_class
   lda_class(trans, proj_dim, f_start, feat_name, opts, i, lda_data_dir, data_dir); 
 end
 
 function lda_class(trans, proj_dim, f_start, feat_name, opts, class_id, dir, data_dir)
+fprintf('%d\n', class_id);
 in_filename = get_data_filename(data_dir, class_id, opts.do_normalize);
 filename = get_data_filename(dir, class_id, opts.do_normalize);
 
-ld = load(in_filename);
-proj = lda(ld.data, trans, proj_dim, f_start);
+fprintf('Loading %s\n', in_filename);
+data = my_read(in_filename, [inf, 512*3*3*384]);
+fprintf('Loaded %s\n', in_filename);
+proj = lda(data, trans, proj_dim, f_start);
+fprintf('Saving %s\n', filename);
 save(filename, 'proj', '-v7.3');
+fprintf('Saved %s\n', filename);
+clear data;
+clear proj;
